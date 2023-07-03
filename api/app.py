7c7a3377f9afb90a44a081f070bf6316336a5821
ShivAnand Y <83615352+hubsMIT1@users.noreply.github.com>
2023-07-03 18:18:20 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -11,7 +11,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 
-app.config['MAIL_USERNAME'] = 'mitashiv0101@gmai.com'
+app.config['MAIL_USERNAME'] = 'mitashiv0101@gmail.com'
 app.config['MAIL_PASSWORD'] = 'lbihwrsjjaznrbft'
 
 # Connect to MongoDB
